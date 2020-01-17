4b7015a2b3c333590cf423bc2f2b3f98d100c689
sobolev_s <sobolevsergeysom@gmail.com>
2020-01-17 02:48:01 +0200

    fix comment

--- app/controllers/promo_messages_recipients_controller.rb
+++ app/controllers/promo_messages_recipients_controller.rb
@@ -5,7 +5,6 @@ class PromoMessageRecipientsController < ApplicationController
     # @note
     # Все проморассылки, которые были отправлены пользователям
     # T.e вывод всех пользователей, которые были отправлены проморассылки с фильтром за период времени
-    # PromoMessageRecipient.filter_by(date_from, date_to)
     # date_from && date_to in ApplicationController private methods
     render json: PromoMessageRecipient.filter_by_time_period(date_from, date_to), status: :ok
   end
